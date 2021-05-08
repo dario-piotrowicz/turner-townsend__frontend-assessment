8cb9f8c60fc63868e300e683258b9fb54ae5de12
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-05-08 12:46:10 +0100

    remove angular boilerplate code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'turner-towsen-frontend-assessment';
-}
+export class AppComponent {}
