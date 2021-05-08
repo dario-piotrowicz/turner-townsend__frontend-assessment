a3c24ab93a4a6e5a7afc3d357039d51a606f4894
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-05-08 18:44:44 +0100

    remove unnecessary app.component.scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import { LoadingService } from './loading.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   public loading: Observable<boolean>;
