1e86c63dde2b480a555afcfe49ad9a57633f6030
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-05-08 22:10:40 +0100

    add fade-in-out animation to loader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,16 +8,9 @@ import { HeaderComponent } from './header/header.component';
 import { LoadingComponent } from './loading/loading.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    LoadingComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, LoadingComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/loading/loading.component.ts
+++ src/app/loading/loading.component.ts
@@ -1,10 +1,23 @@
 import { Component, Input } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-loading',
   templateUrl: './loading.component.html',
   styleUrls: ['./loading.component.scss'],
+  animations: [
+    trigger('loading', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate(250, style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate(250, style({ opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class LoadingComponent {
   @Input() loading: Observable<boolean>;
