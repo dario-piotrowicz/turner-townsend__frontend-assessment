d90f716f927319d442829437b4ff3ea4acf75ef5
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-05-08 18:56:13 +0100

    update the app component spec file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,33 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { of } from 'rxjs';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoadingService } from './loading.service';
+import { LoadingComponent } from './loading/loading.component';
+
+const mockLoadingService = {
+  isLoading: of(true),
+};
 
 describe('AppComponent', () => {
+  let fixture;
+  let app;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent, HeaderComponent, LoadingComponent],
+      providers: [{ provide: LoadingService, useValue: mockLoadingService }],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'turner-towsen-frontend-assessment'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('turner-towsen-frontend-assessment');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('turner-towsen-frontend-assessment app is running!');
+  it('should listen to the loading service isLoading observable', () => {
+    expect(app.loading).toEqual(mockLoadingService.isLoading);
   });
 });
