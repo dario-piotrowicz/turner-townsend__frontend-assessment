4f9debf9edd2ce520bdd82eaf101e520cda118fe
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2021-05-09 11:00:22 +0100

    update the loading service spec file

--- src/app/loading.service.spec.ts
+++ src/app/loading.service.spec.ts
@@ -1,16 +1,23 @@
-import { TestBed } from '@angular/core/testing';
-
+import { take } from 'rxjs/operators';
 import { LoadingService } from './loading.service';
 
 describe('LoadingService', () => {
   let service: LoadingService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(LoadingService);
+    service = new LoadingService();
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should start with _isLoading set to false', () => {
+    expect(service['_isLoading'].value).toEqual(false);
+  });
+
+  it('should update _isLoading and emit with the correct value upon setLoading', () => {
+    const _isLoading = service['_isLoading'];
+    service.setLoading(true);
+    expect(_isLoading.value).toEqual(true);
+    service.isLoading
+      .pipe(take(1))
+      .subscribe((value) => expect(value).toEqual(true));
   });
 });
